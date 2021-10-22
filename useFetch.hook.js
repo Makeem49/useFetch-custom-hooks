ffb0856ed88ad511fc0d6677dd580c5e60c97f28
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-10-22 02:28:34 +0100

    Adding a custome useFetch hook for performing different operation

---
+++ useFetch.hook.js
@@ -0,0 +1,51 @@
+import { func } from 'prop-types';
+import React, {useState} from 'react';
+
+export default function useFetch(baseURL) {
+    const [isLoading, setIsLoading] = useState(true)
+
+    function get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(baseURL + url )
+            .then(response = response.json())
+            .then(data => {
+                if(!data) {
+                    setIsLoading(false)
+                    return reject(data)
+                }
+                setIsLoading(false)
+                resolve(data)
+            })
+            .catch(error => {
+                setIsLoading(false)
+                reject(error)
+            })
+        })
+    }
+
+
+    function post(url, body) {
+        return new Promise((resolve, reject) => {
+            fetch(baseURL + url, {
+                method : 'post',
+                "Content-Type" : "Application/json",
+                body : JSON.stringify(body)
+            })
+            .then(response = response.json())
+            .then(data => {
+                if (!data) {
+                    setIsLoading(false)
+                    return reject(data)
+                }
+                setIsLoading(false)
+                resolve(data)
+            })
+            .catch(error => {
+                setIsLoading(false)
+                reject(error)
+            })
+        })
+    }
+
+    return {get: get, post : post , isLoading}
+}
